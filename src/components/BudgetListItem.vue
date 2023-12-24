de6f0c6b2a20e3f6318fb7fbbc14229934984dea
Michail <36785100+AcademeG1@users.noreply.github.com>
2023-12-24 22:58:37 +0300

    feat: added a dialog box before deleting using element ui

--- src/components/BudgetListItem.vue
+++ src/components/BudgetListItem.vue
@@ -3,9 +3,20 @@
     <div class="list-item" v-for="(item, prop) in list" :key="prop">
       <span class="budget-comment">{{ item.comment }}</span>
       <span class="budget-value">{{ item.value }}</span>
-      <ElButton type="danger" size="mini" @click="onDeleteItem(item.id)"
+      <ElButton type="danger" size="mini" @click="dialogVisible = true"
         >Delete</ElButton
       >
+      <ElDialog title="Delete" :visible.sync="dialogVisible" width="30%">
+        <span>Delete an item?</span>
+        <span slot="footer" class="dialog-footer">
+          <ElButton @click="dialogVisible = false">Cancel</ElButton>
+          <ElButton
+            type="primary"
+            @click="(dialogVisible = false), handleClose(item.id)"
+            >Confirm</ElButton
+          >
+        </span>
+      </ElDialog>
     </div>
   </div>
 </template>
@@ -19,8 +30,15 @@ export default {
       default: () => ({}),
     },
   },
+  data: () => ({
+    dialogVisible: false,
+  }),
   methods: {
-    onDeleteItem(id) {
+    // onDeleteItem(id) {
+    // this.$emit("onDeleteItem", id);
+    // },
+    handleClose(id) {
+      console.log(id);
       this.$emit("onDeleteItem", id);
     },
   },
--- src/components/TotalBalance.vue
+++ src/components/TotalBalance.vue
@@ -1,26 +1,35 @@
 <template>
-  <div class="total-value">
-    {{totalBalance}}
+  <div
+    class="total-value"
+    :class="totalBalance > 0 ? 'green' : totalBalance < 0 ? 'red' : ''"
+  >
+    {{ totalBalance }}
   </div>
 </template>
 
 <script>
 export default {
-  name: 'TotalBalance',
+  name: "TotalBalance",
   props: {
     totalBalance: {
       type: Number,
       default: 0,
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style>
-  .total-value {
-    font-size: 26px;
-    text-transform: uppercase;
-    padding: 20px;
-    text-align: center;
-  }
+.total-value {
+  font-size: 26px;
+  text-transform: uppercase;
+  padding: 20px;
+  text-align: center;
+}
+.green {
+  color: green;
+}
+.red {
+  color: red;
+}
 </style>
